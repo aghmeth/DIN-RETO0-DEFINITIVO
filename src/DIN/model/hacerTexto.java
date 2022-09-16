e8f9304c344305f951d2527798284588bef241f8
belloburgoaalejandro@gmail.com <>
2022-09-16 14:13:54 +0200

    prueba

---
+++ src/DIN/model/hacerTexto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DIN.model;
+
+import java.io.File;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ale
+ */
+public class hacerTexto implements Serializable{
+    File fich = new File("ficheroSaludo.txt");
+    
+    
+}
