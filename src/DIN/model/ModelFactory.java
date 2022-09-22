075d4237acb39a6371b4d4a66f9a4c3d64b29884
belloburgoaalejandro@gmail.com <>
2022-09-22 14:23:37 +0200

    hemos creado el ModelFactory, hemos llamado el método del ModelFactory a la implementación del modelo y tambien hemos credo un fichero

--- src/DIN/model/ModelFactory.java
+++ src/DIN/model/ModelFactory.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DIN.model;
 
-/**
- *
- * @author Ale
- */
+import java.util.ResourceBundle;
+import DIN.model.ModeloImplementacion;
+
 public class ModelFactory {
+    private ResourceBundle data;
+    private final String BD = "BD_type";
+    private final String File = "file_type";
     
+    public String getModel() {
+      String data = ResourceBundle.getBundle("Utilidades.PropiedadSaludo").getString("BD_type");
+        if(BD.equals(data)){
+            ModeloImplementacion modeloBD = new ModeloImplementacion();
+            return modeloBD.toString();
+        }else if(File.equals(data)) {
+            ModeloImplementacion modeloFile = new ModeloImplementacion();
+            return modeloFile.toString();
+        } 
+    return getModel();
+    }
 }
